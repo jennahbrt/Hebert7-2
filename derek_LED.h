6557a2891652d7da0a0ffa685a939cc8a84d11c8
jennahbrt <debian@beaglebone.localdomain>
2021-11-05 00:43:38 +0000

    Uploaded files

---
+++ derek_LED.h
@@ -0,0 +1,25 @@
+#ifndef MAKELED_h
+#define MAKELED_H
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include <time.h>
+#include <unistd.h>
+using namespace std;
+class LED{
+	private: 
+      	  string path;
+	  int number;
+	  virtual void writeLED(string filename, string value);
+	  virtual void removeTrigger();
+	public:
+	LED(int number);
+          virtual void turnOn();
+          virtual void turnOff();
+          virtual void flash(string delayms);
+          virtual void outputState();
+          virtual void blink(int num);
+	  virtual ~LED(); 
+	};
+#endif
